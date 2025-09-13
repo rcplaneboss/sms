de65e52f0a02beac0a293bb782bef7c59f1d5fbe
rcplaneboss <qosimabdulrahman0@gmail.com>
2025-09-13 07:49:44 -0700

    Fix deployment error

--- app/(public)/apply-teacher/page.tsx
+++ app/(public)/apply-teacher/page.tsx
@@ -1,25 +1,25 @@
-"use client";
+// "use client";
 
-import TeacherRegister from "@/components/TeacherTabsClient";
-import { useSearchParams } from "next/navigation";
+// import TeacherRegister from "@/components/TeacherTabsClient";
+// import { useSearchParams } from "next/navigation";
 
-export default function TeacherRegisterPage() {
-  const searchParams = useSearchParams();
-  const vacancyId = searchParams.get("vacancyId"); 
+// export default function TeacherRegisterPage() {
+//   const searchParams = useSearchParams();
+//   const vacancyId = searchParams.get("vacancyId"); 
 
-  if (!vacancyId) {
-    return (
-      <main className="min-h-screen flex items-center justify-center">
-        <p className="text-red-500 font-semibold">
-          ❌ No vacancy selected. Please apply through a vacancy link.
-        </p>
-      </main>
-    );
-  }
+//   if (!vacancyId) {
+//     return (
+//       <main className="min-h-screen flex items-center justify-center">
+//         <p className="text-red-500 font-semibold">
+//           ❌ No vacancy selected. Please apply through a vacancy link.
+//         </p>
+//       </main>
+//     );
+//   }
 
-  return (
-    <main className="min-h-screen flex items-center justify-center">
-      <TeacherRegister vacancyId={vacancyId} />
-    </main>
-  );
-}
+//   return (
+//     <main className="min-h-screen flex items-center justify-center">
+//       <TeacherRegister vacancyId={vacancyId} />
+//     </main>
+//   );
+// }
